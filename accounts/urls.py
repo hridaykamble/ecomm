5f9d180305c95963a88956ebfd831998b7eb3624
hridaykamble <hriday2803@gmail.com>
2023-09-22 17:37:33 +0530

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from accounts.views import login_page, register_page, activate_email,cart,add_to_cart,remove_cart, remove_coupon
+#from accounts.views import 
+
+urlpatterns = [
+    path('login/',login_page, name='login'),
+    path('register/', register_page,name='register'),
+    path('activate/<email_token>', activate_email, name='activate_email'),
+    path('cart/', cart, name='cart'),
+    path('add-to-cart/<uid>/', add_to_cart, name="add_to_cart"),
+    path('remove-cart/<str:cart_item_uid>', remove_cart, name='remove_cart'),
+    path('remove-coupon/<cart_id>/', remove_coupon, name='remove_coupon'),
+]
